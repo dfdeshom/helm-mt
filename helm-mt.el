07d890346fca5bc7a6b97b17940f06e4e79f4305
dfdeshom <didier@cogtree.com>
2015-08-13 00:14:42 -0500

    upgrade to version 0.5

--- helm-mt.el
+++ helm-mt.el
@@ -17,8 +17,7 @@
 
 ;; Author: Didier Deshommes <dfdeshom@gmail.com>
 ;; URL: https://github.com/dfdeshom/helm-mt
-;; Version: 20150302.1804
-;; X-Original-Version: 0.4
+;; Version: 0.5
 ;; Package-Requires: ((emacs "24") (helm "0.0") (multi-term "0.0") (cl-lib "0.5"))
 ;; Keywords: helm multi-term
 
