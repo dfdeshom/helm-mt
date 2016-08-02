29f48d2d90b3c8f27fa9c110d3c5dc1d054af21b
Fritz Grabo <fritz.grabo@gmail.com>
2016-08-02 11:27:18 +0200

    Clean up whitespace
    - remove superfluous blank lines
    - fix mixed in leading tabs in favor of spaces
    - fix indentation

--- helm-mt.el
+++ helm-mt.el
@@ -44,13 +44,13 @@
 
 (defvar helm-mt/all-terminal-modes '(term-mode shell-mode)
   "If a buffer has a major mode in this list, helm-mt will list it as an option.
-The order of the modes controls which is the default action in the helm-mt UI." )
+The order of the modes controls which is the default action in the helm-mt UI.")
 
 (defun helm-mt/terminal-buffers ()
   "Filter for buffers that are terminals only."
   (cl-loop for buf in (buffer-list)
            if (member (buffer-local-value 'major-mode buf) helm-mt/all-terminal-modes)
-           collect (buffer-name buf)) )
+           collect (buffer-name buf)))
 
 (defun helm-mt/unique-buffer-name (name mode)
   "Unique buffer from NAME and MODE."
@@ -58,14 +58,12 @@ The order of the modes controls which is the default action in the helm-mt UI." 
     ('term-mode
      (generate-new-buffer-name (format "*terminal<%s>*" name)))
     ('shell-mode
-     (generate-new-buffer-name (format "*shell<%s>*" name))))
-  )
+     (generate-new-buffer-name (format "*shell<%s>*" name)))))
 
 (defun helm-mt/new-term (name)
   "Create terminal NAME."
   (multi-term)
-  (rename-buffer name)
-  )
+  (rename-buffer name))
 
 (defun helm-mt/launch-term (name mode)
   "Create new terminal in a buffer called NAME using optional MODE."
@@ -74,7 +72,7 @@ The order of the modes controls which is the default action in the helm-mt UI." 
     ('term-mode
      (helm-mt/new-term (helm-mt/unique-buffer-name name 'term-mode)))
     ('shell-mode
-     (shell (helm-mt/unique-buffer-name name 'shell-mode) ))))
+     (shell (helm-mt/unique-buffer-name name 'shell-mode)))))
 
 (defun helm-mt/delete-marked-terms (ignored)
   "Delete marked terminals.  The IGNORED argument is not used."
@@ -82,41 +80,37 @@ The order of the modes controls which is the default action in the helm-mt UI." 
          (len (length buffers)))
     (with-helm-display-marked-candidates
       helm-marked-buffer-name
-          ;; kill the process in the buffer
-          ;; then delete buffer, to avoid confirmation questions
-        (cl-dolist (b buffers)
-          (delete-process b)
-          (kill-buffer b))
-        ;; restore orignal window configuration
-        (balance-windows (selected-frame))
-        (message "%s Terminals deleted" len))))
+      ;; kill the process in the buffer
+      ;; then delete buffer, to avoid confirmation questions
+      (cl-dolist (b buffers)
+        (delete-process b)
+        (kill-buffer b))
+      ;; restore orignal window configuration
+      (balance-windows (selected-frame))
+      (message "%s Terminals deleted" len))))
 
 (defun helm-mt/helper-auto-terminal ()
   "Launch a term with the current directory as the name."
   (let* ((name (replace-regexp-in-string  (regexp-quote "Directory ") "" (pwd)))
          (terminal_name (helm-mt/unique-buffer-name name 'term-mode)))
-    
-    (helm-mt/new-term terminal_name)
-  
-    )
-  )
+    (helm-mt/new-term terminal_name)))
 
 (defun helm-mt/auto-terminal ()
   "Launch a term with the current directory as the name."
   (interactive)
   (helm-mt/helper-auto-terminal)
-  
-                                        ;(helm-keyboard-quit)
-                                        ;(helm-exit-minibuffer)
-                                        ;(exit-minibuffer)
-;  (helm-quit-and-execute-action 'helm-mt/helper-launch-term-with-named-dir)
-    )
+  ;;(helm-keyboard-quit)
+  ;;(helm-exit-minibuffer)
+  ;;(exit-minibuffer)
+  ;;(helm-quit-and-execute-action 'helm-mt/helper-launch-term-with-named-dir)
+  )
 
 (defvar helm-mt/keymap
   (let ((map (make-sparse-keymap)))
     (set-keymap-parent map helm-map)
     (define-key map (kbd "C-c n") 'helm-mt/auto-terminal)
-    (delq nil map)) "Keymap for helm-mt.")
+    (delq nil map))
+  "Keymap for helm-mt.")
 
 
 (defun helm-mt/term-source-terminals ()
@@ -133,11 +127,11 @@ The order of the modes controls which is the default action in the helm-mt UI." 
 
 (defun helm-mt/shell-advice (orig-fun &rest args)
   "Advice that has helm-mt run when invoking `M-x shell` or `M-x term`.
-Agument ORIG-FUN is the original function, ARGS are its arguments"
+Agument ORIG-FUN is the original function, ARGS are its arguments."
   (message "wrapping shell with helm-mt")
   (if (called-interactively-p 'interactive)
-	  (call-interactively 'helm-mt)
-	(apply orig-fun args)))
+      (call-interactively 'helm-mt)
+    (apply orig-fun args)))
 
 ;;;###autoload
 (defun helm-mt/wrap-shells (onoff)
@@ -146,18 +140,17 @@ This routes to helm-mt UI instead of launching a new shell/terminal.
 If ONOFF is t, activate the advice and if nil, remove it."
   (interactive)
   (dolist (mode helm-mt/all-terminal-modes)
-	(let ((fun (intern (replace-regexp-in-string  (regexp-quote "-mode") "" (symbol-name mode)))))
-	  (if onoff
-		  (eval
-		   `(add-function :around (symbol-function (quote ,fun)) #'helm-mt/shell-advice))
-		(eval `(advice-remove (quote ,fun) #'helm-mt/shell-advice))))))
+    (let ((fun (intern (replace-regexp-in-string (regexp-quote "-mode") "" (symbol-name mode)))))
+      (if onoff
+          (eval
+           `(add-function :around (symbol-function (quote ,fun)) #'helm-mt/shell-advice))
+        (eval `(advice-remove (quote ,fun) #'helm-mt/shell-advice))))))
 
 (defun helm-mt/dir-name ()
   "Get the name of the current directory only, not the full name."
   (car (last  (split-string
-           (replace-regexp-in-string
-            (regexp-quote "Directory ") "" (pwd)) "/") 2))
-  )
+               (replace-regexp-in-string
+                (regexp-quote "Directory ") "" (pwd)) "/") 2)))
 
 ;;;###autoload
 (defun helm-mt ()
@@ -172,9 +165,8 @@ If ONOFF is t, activate the advice and if nil, remove it."
                                     (helm-mt/launch-term candidate (quote ,mode)))))
                               helm-mt/all-terminal-modes)))))
     (helm :sources sources
-                                        ;:input (helm-mt/dir-name)
+          ;;:input (helm-mt/dir-name)
           :buffer "*helm-mt*")))
 
 (provide 'helm-mt)
-
 ;;; helm-mt.el ends here
